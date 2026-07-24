23344dd8faf9b41b13cf5cc42c0683dc69c31b2a
ayushmishra-8 <ayushmishra.official9@gmail.com>
2026-07-24 17:24:18 +0530

    Day 002: Two Sum solution

---
+++ Day_002_Two_Sum/Solution.java
@@ -0,0 +1,15 @@
+package Day_002_Two_Sum;
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int k = 1;// we already know we have atleast 1 unique element.Here,k counts unique
+                  // elements.
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[j - 1]) {
+                nums[k] = nums[j];
+                k++;
+            }
+        }
+        return k;
+    }
+}
